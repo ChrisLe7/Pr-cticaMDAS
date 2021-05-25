04eeea008543e8b09014f339317afcf2921c2a35
ChrisLe7 <43857428+ChrisLe7@users.noreply.github.com>
2021-05-25 17:34:38 +0200

    Actualización Interfaz SocioMgt

--- Practica_MDAS/src/es/uco/mdas/business/socio/SocioMgt.java
+++ Practica_MDAS/src/es/uco/mdas/business/socio/SocioMgt.java
@@ -1,13 +1,56 @@
 package es.uco.mdas.business.socio;
 
+import java.util.HashMap;
+
+import es.uco.mdas.datos.SocioDAO;
+import es.uco.mdas.datos.SocioDAOImpFicheros;
+
 public class SocioMgt {
 
-	public DetallesSocio obtenerInformaciónAbono (String idSocio) {
-		DetallesSocio socio = new DetallesSocio();
+
+	
+	
+	public boolean registrarDatosCliente (DetallesCliente cliente) {
+		String idSocio = "";
+		DetallesSocio socio = new DetallesSocio(idSocio, cliente);
+		
+		SocioDAO socioDAO = new SocioDAOImpFicheros();
+		return socioDAO.insertSocio(socio);
+	}
+	
+	public boolean eliminarDatosCliente (String idSocio ) {
+		SocioDAO socioDAO = new SocioDAOImpFicheros();
+		return socioDAO.deleteSocio(idSocio);
+	}  
+	
+	
+	public boolean asignarCategoria (DetallesSocio socio, Categoria categoria) {
+		
+		socio.setCategoria(categoria);
+		
+		return this.updateSocio(socio);
+		
+	}
+
+	
+	public HashMap<String, DetallesSocio> getSocios () {
+	
 		// Crear una clase que devuelve el listado de socios ya sean leidos de una BD o de un fichero
 		
+		SocioDAO socioDAO = new SocioDAOImpFicheros();
 		
-		HashMap<String, DetallesSocio> listadoClientes =  
+		HashMap<String, DetallesSocio> listadoSocios = socioDAO.querySocios();
+		
+		return listadoSocios;
 	}
+	
+	
+	public boolean updateSocio (DetallesSocio socio) {
+		SocioDAO socioDAO = new SocioDAOImpFicheros();
+		
+		return socioDAO.updateSocio(socio);
+			
+	}
+	
 
 }
