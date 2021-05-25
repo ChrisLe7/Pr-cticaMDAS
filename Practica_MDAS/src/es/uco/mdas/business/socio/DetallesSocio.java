4ec994585d70ecfcff15a7b1a35bcf6d76861876
ChrisLe7 <i82luesc@uco.es>
2021-05-25 14:01:38 +0200

    Creación y Corrección de Errores

--- Practica_MDAS/src/es/uco/mdas/business/socio/DetallesSocio.java
+++ Practica_MDAS/src/es/uco/mdas/business/socio/DetallesSocio.java
@@ -5,18 +5,13 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.Date;
 
-public class DetallesSocio implements Serializable {
+public class DetallesSocio  extends DetallesCliente implements Serializable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
 	private String idSocio;
-	private String nombreSocio;
-	private String apellidosSocio;
-	private String direccion;
-	private String telefonoContacto;
-	private Date edad;
 	private Date antiguedad;
 	
 	/**
@@ -33,13 +28,8 @@ public class DetallesSocio implements Serializable {
 	
 	public DetallesSocio(String idSocio, String nombreSocio, String apellidosSocio, String direccion,
 			String telefonoContacto, Date edad, Date antiguedad) {
-		super();
+		super(nombreSocio, apellidosSocio,direccion,telefonoContacto,edad);
 		this.idSocio = idSocio;
-		this.nombreSocio = nombreSocio;
-		this.apellidosSocio = apellidosSocio;
-		this.direccion = direccion;
-		this.telefonoContacto = telefonoContacto;
-		this.edad = edad;
 		this.antiguedad = antiguedad;
 	}
 	
@@ -71,110 +61,7 @@ public class DetallesSocio implements Serializable {
 		this.idSocio = idSocio;
 	}
 	
-
-	/**
-	 * Devuelve el Nombre del Socio
-	 * @return Nombre del Socio
-	 * 
-	 * */
-		
-	public String getNombreSocio() {
-		return nombreSocio;
-	}
 	
-	/**
-	 * Asigna un Nombre al socio
-	 * 
-	 * @param nombreSocio Nombre a asignar al socio
-	 * */
-	
-	public void setNombreSocio(String nombreSocio) {
-		this.nombreSocio = nombreSocio;
-	}
-	
-	/**
-	 * Devuelve los Apellidos del Socio
-	 * @return Apellidos del Socio
-	 * 
-	 * */	
-	
-	public String getApellidosSocio() {
-		return apellidosSocio;
-	}
-	
-	/**
-	 * Asigna los Apellidos al socio
-	 * 
-	 * @paramr apellidosSocio Apellidos a asignar al socio.
-	 * */
-	
-	public void setApellidosSocio(String apellidosSocio) {
-		this.apellidosSocio = apellidosSocio;
-	}
-	
-	
-	/**
-	 * Devuelve la Direccion de Contacto del Socio
-	 * @return Direccion del cliente
-	 * */
-	public String getDireccion() {
-		return direccion;
-	}
-	
-	/**
-	 * Asigna la direccion de contacto al socio.
-	 * @param direccion Direccion de contacto del socio a asignar.
-	 * */
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-	
-	/**
-	 * Devuelve el Telefono de Contacto del Socio
-	 * @return Telefono contacto del cliente
-	 * */
-	
-	public String getTelefonoContacto() {
-		return telefonoContacto;
-	}
-	
-	
-	/**
-	 * Asigna el telefono de contacto al socio.
-	 * @param telefonoContacto Telefono de contacto del socio a asignar.
-	 * */
-	public void setTelefonoContacto(String telefonoContacto) {
-		this.telefonoContacto = telefonoContacto;
-	}
-
-	/**
-	 * Devuelve la fecha de Nacimiento del Socio
-	 * @return Telefono contacto del cliente
-	 * */
-	
-	public Date getFechaNacimiento() {
-		return edad;
-	}
-
-	/**
-	 * Asigna el telefono de contacto al socio.
-	 * @param telefonoContacto Telefono de contacto del socio a asignar.
-	 * */
-	
-	public void setFechaNacimiento(Date edad) {
-		this.edad = edad;
-	}
-	
-	
-	/**
-	 * Devuelve la edad que tiene el Socio
-	 * @return Edad del Socio
-	 * */
-	public int getEdad() {
-		Period edad = Period.between(LocalDate.ofEpochDay(this.edad.getTime()), LocalDate.now());
-		return edad.getYears();
-	}
-
 	/**
 	 * Devuelve la fecha de inicio de la creación del Socio
 	 * @return Fecha del Creacion del Socio
@@ -196,7 +83,7 @@ public class DetallesSocio implements Serializable {
 	 * @return Tiempo siendo socio
 	 * */
 	public int getAntiguedad() {
-		Period tiempo = Period.between(LocalDate.ofEpochDay(this.edad.getTime()), LocalDate.now());
+		Period tiempo = Period.between(LocalDate.ofEpochDay(this.antiguedad.getTime()), LocalDate.now());
 		return tiempo.getYears();
 	}
 
