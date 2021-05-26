413e97df878c1e52421702d2e27f382d13108a57
ChrisLe7 <43857428+ChrisLe7@users.noreply.github.com>
2021-05-26 12:10:16 +0200

    Actualización de la implementación de IGestionarAbono

--- Practica_MDAS/src/es/uco/mdas/business/instalaciondeportiva/impl/InstalacionDeportivaImpl.java
+++ Practica_MDAS/src/es/uco/mdas/business/instalaciondeportiva/impl/InstalacionDeportivaImpl.java
@@ -1,41 +1,88 @@
 package es.uco.mdas.business.instalaciondeportiva.impl;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
 import es.uco.mdas.business.instalaciondeportiva.DetallesLocalidad;
 import es.uco.mdas.business.instalaciondeportiva.GestionarAbono;
 import es.uco.mdas.business.socio.DetallesAbono;
+import es.uco.mdas.datos.AbonoDAO;
+import es.uco.mdas.datos.LocalidadDAO;
 
 public class InstalacionDeportivaImpl implements GestionarAbono {
 
 	@Override
 	public ArrayList<DetallesLocalidad> getLocalidadesDisponibles() {
-		// TODO Auto-generated method stub
-		return null;
+		LocalidadDAO localidadDAO = new LocalidadDAOImpFicheros();
+		
+
+		HashMap<String, DetallesLocalidad> listadoLocalidades = localidadDAO.queryAll();
+		Collection<DetallesLocalidad> localidades = listadoLocalidades.values();
+		
+		ArrayList<DetallesLocalidad> localidadesDisponibles = new ArrayList<> (localidades);
+		
+		return localidadesDisponibles;
 	}
 
 	@Override
 	public void asignarLocalidad(DetallesLocalidad localidad, String idSocio) {
-		// TODO Auto-generated method stub
+		localidad.setIdSocio(idSocio);
+		LocalidadDAO localidadDAO = new LocalidadDAOImpFicheros();
+		
+		localidadDAO.update(localidad);
+		
 		
 	}
 
 	@Override
 	public void liberarLocalidad(DetallesLocalidad localidad) {
-		// TODO Auto-generated method stub
+		localidad.setIdSocio("");
+		LocalidadDAO localidadDAO = new LocalidadDAOImpFicheros();
+		
+		localidadDAO.update(localidad);
 		
 	}
 
 	@Override
 	public boolean liberarAbono(String idAbono) {
 		// TODO Auto-generated method stub
-		return false;
+		boolean estado = false;
+		AbonoDAO abonoDAO = new AbonoDAOImpFicheros();
+		
+		DetallesAbono informacionAbono = abonoDAO.queryAbono(idAbono);
+		
+		String idLocalidad = informacionAbono.getIdLocalidad();
+		
+		LocalidadDAO localidadDAO = new LocalidadDAOImpFicheros();
+		
+		DetallesLocalidad informacionLocalidad = localidadDAO.queryById(idLocalidad);
+		
+		if (informacionLocalidad != null) {
+			this.liberarLocalidad(informacionLocalidad);
+			estado = !estado;
+		}
+		
+		return estado;
 	}
 
 	@Override
-	public void registrarAbono(DetallesAbono abono) {
+	public boolean registrarAbono(DetallesAbono abono) {
 		// TODO Auto-generated method stub
+		boolean estado = false;
+	
+		String idLocalidad = abono.getIdLocalidad();
 		
+		LocalidadDAO localidadDAO = new LocalidadDAOImpFicheros();
+		
+		DetallesLocalidad informacionLocalidad = localidadDAO.queryById(idLocalidad);
+		
+		if (informacionLocalidad.getIdSocio() == "" ) {
+			informacionLocalidad.setIdSocio(abono.getIdAbono());
+			estado = !estado;
+		}
+		
+		return estado;
 	}
 
 }
