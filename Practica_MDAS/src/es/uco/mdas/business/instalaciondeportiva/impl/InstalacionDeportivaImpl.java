e1787c95c3040a46197b02862eaf29dc1c5c10f9
ChrisLe7 <43857428+ChrisLe7@users.noreply.github.com>
2021-05-26 23:02:02 +0200

    Actualización de la implementación de InstalaciónDeportivaMgt

--- Practica_MDAS/src/es/uco/mdas/business/instalaciondeportiva/impl/InstalacionDeportivaImpl.java
+++ Practica_MDAS/src/es/uco/mdas/business/instalaciondeportiva/impl/InstalacionDeportivaImpl.java
@@ -1,16 +1,22 @@
 package es.uco.mdas.business.instalaciondeportiva.impl;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.HashMap;
 
+import es.uco.mdas.business.instalaciondeportiva.DetallesContrato;
+import es.uco.mdas.business.instalaciondeportiva.DetallesEspacioComercial;
 import es.uco.mdas.business.instalaciondeportiva.DetallesLocalidad;
+import es.uco.mdas.business.instalaciondeportiva.DetallesPresupuesto;
 import es.uco.mdas.business.instalaciondeportiva.GestionarAbono;
+import es.uco.mdas.business.instalaciondeportiva.RealizacionContrato;
 import es.uco.mdas.business.socio.DetallesAbono;
 import es.uco.mdas.datos.AbonoDAO;
+import es.uco.mdas.datos.ContratoDAO;
 import es.uco.mdas.datos.LocalidadDAO;
 
-public class InstalacionDeportivaImpl implements GestionarAbono {
+public class InstalacionDeportivaImpl implements GestionarAbono, RealizacionContrato {
 
 	@Override
 	public ArrayList<DetallesLocalidad> getLocalidadesDisponibles() {
@@ -83,6 +89,71 @@ public class InstalacionDeportivaImpl implements GestionarAbono {
 		}
 		
 		return estado;
+	}
+
+	@Override
+	public DetallesPresupuesto generarPresupuesto(DetallesEspacioComercial espacioComercial) {
+		String idPresupuesto = "";
+		
+		String idEspacioAComprar = espacioComercial.getIdEspacio();
+		float precioParaPresupuesto = espacioComercial.getPrecio();
+		DetallesPresupuesto presupuestoGenerado  = new DetallesPresupuesto(idPresupuesto, idEspacioAComprar, precioParaPresupuesto);
+		
+		return presupuestoGenerado;
+	}
+
+	@Override
+	public DetallesContrato realizarContrato(String idEmpresa, String cuentaBancaria, String idEstadio) {
+		
+		return null;
+	}
+
+	@Override
+	public void asignarEspacioComercial(String idEspacioComercial, String idEmpresa) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DetallesContrato renovarContrato(String idContrato) {
+		ContratoDAO contratoDAO = new ContratoDAOImpFicheros();
+		
+		DetallesContrato contratoARenovar = contratoDAO.queryById(idContrato);
+		
+		Calendar fechaRenovacionContrato = contratoARenovar.getFechaRestriccion();
+		int nuevaFecha = fechaRenovacionContrato.get(Calendar.YEAR) + 1;
+		fechaRenovacionContrato.set(Calendar.YEAR, nuevaFecha);
+		contratoARenovar.setFechaRestriccion(fechaRenovacionContrato);
+		
+		contratoDAO.update(contratoARenovar);
+		return contratoARenovar;
+	}
+
+	@Override //PODRIA DEVOLVER UN BOOLEANO
+	public void cancelarContrato(String idContrato) {
+		ContratoDAO contratoDAO = new ContratoDAOImpFicheros();
+		
+		DetallesContrato contratoACancelar = contratoDAO.queryById(idContrato);
+		
+		if (contratoACancelar != null ) {
+			Calendar fechaCancelacion = Calendar.getInstance();
+			contratoACancelar.setFechaRestriccion(fechaCancelacion);
+			
+			contratoDAO.update(contratoACancelar);
+
+		}
+	
+		
+	}
+
+	@Override
+	public void liberarEspacioComercial(String idEspacioComercial) {
+		
+		EspacioComercialDAO espacioComercialDAO = new EspacioComercialDAOImpFicheros();
+		DetallesEspacioComercial espacioComercialLiberar = espacioComercialDAO.queryById(idEspacioComercial);
+		espacioComercialLiberar.setArrendado("");
+		espacioComercialDAO.update(espacioComercialLiberar);
+		
 	}
 
 }
