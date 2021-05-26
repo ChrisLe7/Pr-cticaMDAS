47aea8f535e074074390daa90766100b78ac844f
ChrisLe7 <43857428+ChrisLe7@users.noreply.github.com>
2021-05-26 11:39:38 +0200

    Creación de las interfaces de gestión de los datos del sistema
    Para la interfaz IInstalaciónDeportivaMGT

---
+++ Practica_MDAS/src/es/uco/mdas/datos/DespachoDAO.java
@@ -0,0 +1,9 @@
+package es.uco.mdas.datos;
+
+import es.uco.mdas.business.instalaciondeportiva.DetallesDespacho;
+
+public interface DespachoDAO extends DAO <DetallesDespacho>{
+
+
+	
+}
---
+++ Practica_MDAS/src/es/uco/mdas/datos/EntradaDAO.java
@@ -0,0 +1,7 @@
+package es.uco.mdas.datos;
+
+import es.uco.mdas.business.instalaciondeportiva.DetallesEntrada;
+
+public interface EntradaDAO extends DAO <DetallesEntrada>{
+	
+}
