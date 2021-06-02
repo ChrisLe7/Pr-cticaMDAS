6312fc8df6e9dfaf1170698448cfacb10fdaadf9
almojara <55484237+almojara@users.noreply.github.com>
2021-06-02 18:09:22 +0200

    Creado test para ver todos los socios guardados en el fichero

---
+++ Practica_MDAS/src/es/uco/mdas/tests/TestSocios.java
@@ -0,0 +1,29 @@
+package es.uco.mdas.tests;
+
+import java.util.HashMap;
+
+import es.uco.mdas.business.socio.DetallesSocio;
+import es.uco.mdas.datos.SocioDAO;
+import es.uco.mdas.datos.SocioDAOImpFicheros;
+
+public class TestSocios {
+
+	public static void main(String[] args) {
+		
+		SocioDAO socioDAO = new SocioDAOImpFicheros();
+		
+		System.out.println("TestSocioDAO");
+		
+		HashMap<String, DetallesSocio> socios = socioDAO.queryAll();
+		
+		System.out.println("Se han recuperado " + socios.size() + " socios");
+		
+		for (DetallesSocio socio : socios.values()) {
+			System.out.println(socio.toString());
+		}
+		
+		System.out.println("Exito");
+		
+	}
+	
+}
